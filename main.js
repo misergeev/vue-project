fe723fa34856f52cd46e294c5aa69ed9b1b21641
Michail Sergeev <mialsergeev@gmail.com>
2022-04-25 13:41:01 +0500

    Add delete function

--- main.js
+++ main.js
@@ -2,10 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             items: [
-                {id: 1, name: 'Элемент', descr: 'Описание элемента'},
-                {id: 2, name: 'Элемент', descr: 'Описание элемента'},
+                {id: 1, name: 'Прибраться', descr: 'Нужно пирать комнату'},
+                {id: 2, name: 'Сходить в магазин'},
                 {id: 3, name: 'Элемент', descr: 'Описание элемента'},
             ],
+        }
+    },
+    methods: {
+        deleteElem(element) {
+            let elementId = element.path[1].dataset.id;
+            for (let i = 0; i < this.items.length; i++ ) {
+                if (elementId == this.items[i].id) {
+                    this.items.splice(i, 1)
+                }
+            }
         }
     }
 })
