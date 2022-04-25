22192bbabb1e1de68f5ec7f13f8f110de84ca76d
Michail Sergeev <mialsergeev@gmail.com>
2022-04-25 12:37:40 +0500

    first steps

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            items: [
+                {id: 1, name: 'Элемент', descr: 'Описание элемента'},
+                {id: 2, name: 'Элемент', descr: 'Описание элемента'},
+                {id: 3, name: 'Элемент', descr: 'Описание элемента'},
+            ],
+        }
+    }
+})
